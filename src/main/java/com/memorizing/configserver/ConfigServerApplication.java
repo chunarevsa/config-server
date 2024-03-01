a83762f801b5f34c2c45fc1e16364a8f84cc6726
Chunarev Sergei <chunarev.se.a@gmail.com>
2024-03-01 20:49:13 +0100

    update group

--- src/main/java/com/memorizing/configserver/ConfigServerApplication.java
+++ src/main/java/com/memorizing/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.configserver;
+package com.memorizing.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
